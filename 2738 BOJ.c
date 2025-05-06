528bcdef2396f96e59bd17adf37c9fec879607c3
geosang122 <psh30122@naver.com>
2025-05-06 20:09:05 +0900

    Create 2738 BOJ.c

---
+++ 2738 BOJ.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//Çà·Ä µ¡¼À
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	int a[100][100]; //¹è¿­ ¼±¾ð
+	int b[100][100];
+	for (int i = 0;i < n;i++) {
+		for (int j = 0;j < m;j++) {
+			scanf("%d", &a[i][j]);
+		}
+	}
+	for (int i = 0;i < n;i++) {
+		for (int j = 0;j < m;j++) {
+			scanf("%d", &b[i][j]);
+		}
+	}
+	for (int i = 0;i < n;i++) {
+		for (int j = 0;j < m;j++) {
+			printf("%d ", a[i][j]+b[i][j]);
+		}
+		puts("");
+	}
+	return 0;
+}
