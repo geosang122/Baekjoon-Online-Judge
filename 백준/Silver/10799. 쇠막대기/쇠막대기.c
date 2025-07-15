4fc6194374215e64904f35edc38b0730b8b77646
geosang122 <psh30122@naver.com>
2025-07-15 13:59:31 +0900

    [Silver II] Title: 쇠막대기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.c
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.c
@@ -5,21 +5,17 @@
 int main() {
 	char p[100005] = { 0 };
 	scanf("%s", p);
-	char p2[100005] = { 0 };
-	int i = 1, j = 1, sum = 0;;
-	p2[0] = p[0];
-	for (; p[i]; i++) {
+	int stack = 0,sum=0;
+	for (int i=0; p[i]; i++) {
 		if (p[i] == ')') {
-			if (p[i - 1] == '(') {
-				sum += --j;
-			}
+			if (p[i - 1] == '(') sum += --stack;
 			else {
 				sum += 1;
-				j--;
+				stack--;
 			}
 		}
 		else if (p[i] == '(') {
-			p[j++] = p[i];
+			stack++;
 		}
 	}
 	printf("%d", sum);
