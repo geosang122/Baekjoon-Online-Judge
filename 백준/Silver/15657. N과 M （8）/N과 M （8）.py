b8227afc416b3bde64c48bed655fa81d59372503
geosang122 <psh30122@naver.com>
2025-11-09 19:10:29 +0900

    [Silver III] Title: N과 M (8), Time: 112 ms, Memory: 110976 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,15 @@
+def bt():
+    if len(ary)==m:
+        print(' '.join(map(str,ary)))
+        return
+
+    for i in range(n):
+        if len(ary)==0 or ary[-1]<=num[i]:
+            ary.append(num[i])
+            bt()
+            ary.pop()
+
+n,m=map(int,input().split())
+num=sorted(list(map(int,input().split())))
+ary=[]
+bt()
