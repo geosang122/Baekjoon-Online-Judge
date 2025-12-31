7ab6c11b41efac2041e1ac235fc6f02f0171d5c3
geosang122 <psh30122@naver.com>
2026-01-01 01:28:28 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 356 ms, Memory: 118324 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,5 +1,7 @@
-import sys 
-input=sys.stdin.readline
+import sys
+from bisect import bisect_left, bisect_right
+
+input = sys.stdin.readline
 
 s=input()
 alpha=dict()
@@ -13,10 +15,10 @@ for _ in range(k):
     q=input().split()
     ch=q[0]
     r,l=int(q[1]),int(q[2])
-    chlist=alpha.get(ch)
-    count=0
+    chlist=alpha.get(ch,[])
     if chlist:
-        for j in chlist:
-            if r<=j<=l:
-                count+=1
-    print(count)
+        left_idx = bisect_left(chlist, r)
+        right_idx = bisect_right(chlist, l)
+        print(right_idx - left_idx)
+    else:
+        print(0)
