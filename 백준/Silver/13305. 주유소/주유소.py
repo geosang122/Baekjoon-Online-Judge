1bbc6214b9e07a1ea9d2e27cb1e720e2babbf291
geosang122 <psh30122@naver.com>
2026-01-07 00:52:14 +0900

    [Silver III] Title: 주유소, Time: 128 ms, Memory: 135244 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,25 +1,15 @@
+import sys
+input=sys.stdin.readline
+
 st=int(input())
 dist=list(map(int,input().split()))
 price=list(map(int,input().split()))
-visited=[False]*st
-visited[st-1]=True
-total=0
-while True:
-    distance=0
-    min=1000000000000
-    minidx=-1
-    for i in range(st):
-        if visited[i]:
-            break
-        if price[i]<min:
-            minidx=i
-            min=price[i]
-    if minidx==-1:
-        break
-    for j in range(minidx,st):
-        if visited[j]:
-            break
-        distance+=dist[j]
-        visited[j]=True
-    total+=(distance*min)
-print(total)
+total_cost=0
+min_price=price[0]
+
+for i in range(st-1):
+    if price[i]<min_price:
+        min_price=price[i]
+    total_cost+=min_price*dist[i]
+
+print(total_cost)
