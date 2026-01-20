0d29e7485091c409660965ef4d200e19e9fea5c6
geosang122 <psh30122@naver.com>
2026-01-20 14:00:19 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 460 ms, Memory: 134260 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,23 @@
+from collections import deque
+
+n,m,r=map(int,input().split())
+visited=[0 for _ in range(n+1)]
+count=1
+g=[[] for _ in range(n+1)]
+for _ in range(m):
+    a,b=map(int,input().split())
+    g[a].append(b)
+    g[b].append(a)
+q=deque([r])
+while q:
+    node=q.popleft()
+    if visited[node]==0:
+        visited[node]=count
+        count+=1
+        g[node].sort(reverse=True)
+        for k in g[node]:
+            if visited[k]==0:
+                q.append(k)
+
+for i in range(1,n+1):
+    print(visited[i])
